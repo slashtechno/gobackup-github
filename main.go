61176ba40e5e7f8f25ea284549b2960910a21469
slashtechno <slashtechno@users.noreply.github.com>
2022-06-13 19:24:41 -0400

    Added print statements to output program status

--- main.go
+++ main.go
@@ -27,22 +27,27 @@ func main() {
 	// response := ghRequest("https://api.github.com/user/repos")
 	// fmt.Println(responseContent(response.Body))
 	cloneRepos()
+	fmt.Println("Done!")
 }
 
 func cloneRepos() {
 	currentDirectory, _ := os.Getwd()
 	dateToday := time.Now().Format("01-02-2006")
+	user := gjson.Get(responseContent(ghRequest("https://api.github.com/user").Body), "login")
+	fmt.Println("User: " + user.String())
 	var repoSlice []repoStruct
-	for i := 1; i <= calculateNeededPages(); i++ {
+	neededPages := calculateNeededPages()
+	for i := 1; i <= neededPages; i++ {
+		fmt.Println("Getting list of repositories")
 		repoJSON := responseContent(ghRequest("https://api.github.com/user/repos?page" + strconv.Itoa(i)).Body)
 		err := json.Unmarshal([]byte(repoJSON), &repoSlice)
 		if err != nil {
 			fmt.Printf("Error:\n%v\n", err)
 		}
-		fmt.Println(repoSlice)
+		fmt.Println("Cloning repositories...")
 		for i := 0; i < len(repoSlice); i++ {
 			cloneDirectory := filepath.Dir(currentDirectory + "/github-backup-" + dateToday + "/" + repoSlice[i].Name + "/")
-			user := gjson.Get(responseContent(ghRequest("https://api.github.com/user").Body), "login")
+			fmt.Printf("Cloning %v to %v\n", repoSlice[i].Name, cloneDirectory)
 			_, err = git.PlainClone(cloneDirectory, false, &git.CloneOptions{
 				URL: repoSlice[i].HTMLURL,
 				Auth: &githttp.BasicAuth{
@@ -63,8 +68,10 @@ func calculateNeededPages() int {
 	publicRepos := gjson.Get(json, "public_repos")
 	privateRepos := gjson.Get(json, "total_private_repos")
 	totalRepos := publicRepos.Num + privateRepos.Num
+	fmt.Println("Total repositories: " + strconv.Itoa(int(totalRepos)))
 	neededPages := math.Ceil(totalRepos / 100)
 	// fmt.Println(neededPages)
+	fmt.Println("Total pages needed:" + strconv.Itoa(int(neededPages)))
 	return int(neededPages)
 }
 
@@ -87,6 +94,15 @@ func ghRequest(url string) *http.Response {
 	response, err := http.DefaultClient.Do(req)
 	if err != nil {
 		fmt.Printf("Error:\n%v\n", err)
+	}
+	if response.StatusCode != 200 {
+		fmt.Println("Something went wrong, status code is not \"200 OK\"")
+		fmt.Println("Response status code: " + response.Status)
+		if response.StatusCode == 401 {
+			fmt.Println("You may have an incorrect Github personal token")
+		}
+		os.Exit(1)
+
 	}
 	return response
 }
