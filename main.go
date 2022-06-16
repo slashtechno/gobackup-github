3ad8cd45c187da9885805ec2bea529d44f74f689
slashtechno <77907286+slashtechno@users.noreply.github.com>
2022-06-16 09:50:44 -0500

    Added function to clone starred repos

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -9,6 +10,7 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/go-git/go-git/v5"
@@ -24,10 +26,22 @@ type repoStruct struct {
 
 func main() {
 	godotenv.Load()
-	// response := ghRequest("https://api.github.com/user/repos")
-	// fmt.Println(responseContent(response.Body))
-	cloneRepos()
-	fmt.Println("Done!")
+	fmt.Println(`What should be backed up?
+1) Your public and private repositories
+2) Your starred repositories
+3) Both`)
+	reader := bufio.NewReader(os.Stdin)
+	backupChoice, _ := reader.ReadString('\n')
+	backupChoice = strings.TrimSpace(backupChoice)
+	switch backupChoice {
+	case "1":
+		cloneRepos()
+	case "2":
+		cloneStars()
+	case "3":
+		cloneRepos()
+		cloneStars()
+	}
 }
 
 func cloneRepos() {
@@ -36,18 +50,17 @@ func cloneRepos() {
 	user := gjson.Get(responseContent(ghRequest("https://api.github.com/user").Body), "login")
 	fmt.Println("User: " + user.String())
 	var repoSlice []repoStruct
-	neededPages := calculateNeededPages()
+	neededPages := calculateNeededPages("userRepos")
+	fmt.Println("Getting list of repositories")
 	for i := 1; i <= neededPages; i++ {
-		fmt.Println("Getting list of repositories")
-		repoJSON := responseContent(ghRequest("https://api.github.com/user/repos?page" + strconv.Itoa(i)).Body)
+		repoJSON := responseContent(ghRequest("https://api.github.com/user/repos?per_page=100&page=" + strconv.Itoa(i)).Body)
 		err := json.Unmarshal([]byte(repoJSON), &repoSlice)
 		if err != nil {
 			fmt.Printf("Error:\n%v\n", err)
 		}
-		fmt.Println("Cloning repositories...")
 		for i := 0; i < len(repoSlice); i++ {
-			cloneDirectory := filepath.Dir(currentDirectory + "/github-backup-" + dateToday + "/" + repoSlice[i].Name + "/")
-			fmt.Printf("Cloning %v to %v\n", repoSlice[i].Name, cloneDirectory)
+			cloneDirectory := filepath.Dir(currentDirectory + "/github-backup/your-repos-" + dateToday + "/" + repoSlice[i].Name + "/")
+			fmt.Printf("Cloning %v (iteration %v) to %v\n", repoSlice[i].Name, i+1, cloneDirectory)
 			_, err = git.PlainClone(cloneDirectory, false, &git.CloneOptions{
 				URL: repoSlice[i].HTMLURL,
 				Auth: &githttp.BasicAuth{
@@ -62,17 +75,71 @@ func cloneRepos() {
 	}
 }
 
-func calculateNeededPages() int {
-	response := ghRequest("https://api.github.com/user")
-	json := responseContent(response.Body)
-	publicRepos := gjson.Get(json, "public_repos")
-	privateRepos := gjson.Get(json, "total_private_repos")
-	totalRepos := publicRepos.Num + privateRepos.Num
-	fmt.Println("Total repositories: " + strconv.Itoa(int(totalRepos)))
-	neededPages := math.Ceil(totalRepos / 100)
-	// fmt.Println(neededPages)
-	fmt.Println("Total pages needed:" + strconv.Itoa(int(neededPages)))
-	return int(neededPages)
+func cloneStars() {
+	currentDirectory, _ := os.Getwd()
+	dateToday := time.Now().Format("01-02-2006")
+	user := gjson.Get(responseContent(ghRequest("https://api.github.com/user").Body), "login")
+	fmt.Println("User: " + user.String())
+	var starSlice []repoStruct
+	neededPages := calculateNeededPages("userStars")
+	fmt.Println("Getting list of starred repositories")
+	for i := 1; i <= neededPages; i++ {
+		repoJSON := responseContent(ghRequest("https://api.github.com/user/starred?per_page=100&page=" + strconv.Itoa(i)).Body)
+		err := json.Unmarshal([]byte(repoJSON), &starSlice)
+		if err != nil {
+			fmt.Printf("Error:\n%v\n", err)
+		}
+		for i := 0; i < len(starSlice); i++ {
+			cloneDirectory := filepath.Dir(currentDirectory + "/github-backup/your-stars-" + dateToday + "/" + starSlice[i].Name + "/")
+			fmt.Printf("Cloning %v (iteration %v) to %v\n", starSlice[i].Name, i, cloneDirectory)
+			_, err = git.PlainClone(cloneDirectory, false, &git.CloneOptions{
+				URL: starSlice[i].HTMLURL,
+				Auth: &githttp.BasicAuth{
+					Username: user.String(), // anything except an empty string
+					Password: loadToken(),
+				},
+			})
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+}
+
+func calculateNeededPages(whichRepos string) int {
+	if whichRepos == "userRepos" {
+		response := ghRequest("https://api.github.com/user")
+		json := responseContent(response.Body)
+		publicRepos := gjson.Get(json, "public_repos")
+		privateRepos := gjson.Get(json, "total_private_repos")
+		totalRepos := publicRepos.Num + privateRepos.Num
+		fmt.Println("Total repositories: " + strconv.Itoa(int(totalRepos)))
+		neededPages := math.Ceil(totalRepos / 100)
+		// fmt.Println(neededPages)
+		fmt.Println("Total pages needed:" + strconv.Itoa(int(neededPages)))
+		return int(neededPages)
+	} else if whichRepos == "userStars" {
+		var starSlice []repoStruct
+		pageNumber := 1
+		perPage := 100
+		starJSON := responseContent(ghRequest("https://api.github.com/user/starred?page=1&per_page=" + strconv.Itoa(perPage)).Body)
+		err := json.Unmarshal([]byte(starJSON), &starSlice)
+		if err != nil {
+			fmt.Printf("Error:\n%v\n", err)
+		}
+		for len(starSlice) != 0 { // if len(starSlice) == perPage {
+			pageNumber++
+			starJSON := responseContent(ghRequest("https://api.github.com/user/starred?page=" + strconv.Itoa(pageNumber) + "&per_page=" + strconv.Itoa(perPage)).Body)
+			err := json.Unmarshal([]byte(starJSON), &starSlice)
+			if err != nil {
+				fmt.Printf("Error:\n%v\n", err)
+			}
+
+		}
+		return pageNumber
+	} else {
+		return 0
+	}
 }
 
 func responseContent(responseBody io.ReadCloser) string {
